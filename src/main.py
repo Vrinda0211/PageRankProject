32acc8da366985ca6fbb485b554da20c98394ce9
Vrinda Venugopal <vrindavenugopal2005@gmail.com>
2026-03-22 10:55:22 +0530

    graph and adj matrix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,32 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-print("Setup successful")
+
+def read_graph(file_path):
+    graph={}
+    with open(file_path,"r") as f:
+        for line in f:
+            parts=line.strip().split("->")
+            node=parts[0].strip()
+
+            if len(parts)>1:
+                links=parts[1].strip().split()
+            else:
+                links=[]
+            graph[node]=links
+    return graph
+graph=read_graph("data\graph.txt")
+print(graph)
+
+def adj_mat(graph):
+    nodes=list(graph.keys())
+    n=len(nodes)
+    index={node: i for i,node in enumerate(nodes)}
+    A=np.zeros((n,n))
+    for node,links in graph.items():
+        j=index[node]
+        for link in links:
+            if link in index:
+                i=index[link]
+                A[i][j]=1
+    return A,nodes
